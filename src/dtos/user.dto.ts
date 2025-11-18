2aab92413e6e69853a49165fe38616baa4f9eba4
helangeorge999 <helangeorge78@gmail.com>
2025-11-18 08:12:16 +0545

    first commit

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { UserSchema } from '../types/user.types';
+
+// what clients send when creating a user
+export const CreateUserDto = UserSchema.extend({
+    password: z.string().min(6),
+});
+
+export type CreateUserDto = z.infer<typeof CreateUserDto>;
+//what server responds with when sending user data
+export const UserResponseDto = z.object({
+    id: z.string(),
+    name: z.string(),
+    email: z.email(),
+    createAt: z.date(),
+
+})
+export  type UserResponseDto = z.infer<typeof UserResponseDto>;
